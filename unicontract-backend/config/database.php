4e403cb76284fc2a2e2e627cc3f55d21a3d373bb
Francesco Filicetti <easyweb.wa@gmail.com>
2022-09-30 13:04:51 +0200

    fix: db config

--- unicontract-backend/config/database.php
+++ unicontract-backend/config/database.php
@@ -93,24 +93,10 @@ return [
             'password'       => env('DB_PASSWORD_ORACLE', ''),
             // 'charset'        => env('DB_CHARSET', 'AL32UTF8'),
             'prefix'         => env('DB_PREFIX', ''),
-            'prefix_schema'  => env('DB_SCHEMA_PREFIX', ''),
+            // 'prefix_schema'  => env('DB_SCHEMA_PREFIX', ''),
             // 'server_version' => env('DB_SERVER_VERSION', '11g'),
         ],
 
-        'oracle_siaru' => [
-            'driver'         => 'oracle',
-            'tns'            => env('DB_TNS', ''),
-            // 'host'           => env('DB_HOST', ''),
-            // 'port'           => env('DB_PORT', '1521'),
-            'database'       => env('DB_DATABASE', 'xe'),
-            'service_name'   => env('DB_SERVICENAME', ''),
-            'username'       => env('DB_USERNAME_ORACLE', ''),
-            'password'       => env('DB_PASSWORD_ORACLE', ''),
-            // 'charset'        => env('DB_CHARSET', 'AL32UTF8'),
-            'prefix'         => env('DB_PREFIX', ''),
-            'prefix_schema'  => env('DB_SCHEMA_PREFIX_SIARU', ''),
-            // 'server_version' => env('DB_SERVER_VERSION', '11g'),
-        ],
 
     ],
 
--- unicontract-backend/config/oracle.php
+++ unicontract-backend/config/oracle.php
@@ -14,7 +14,7 @@ return [
         'password'       => env('DB_PASSWORD_ORACLE', ''),
         // 'charset'        => env('DB_CHARSET', 'AL32UTF8'),
         'prefix'         => env('DB_PREFIX', ''),
-        'prefix_schema'  => env('DB_SCHEMA_PREFIX', ''),
+        // 'prefix_schema'  => env('DB_SCHEMA_PREFIX', ''),
         // 'server_version' => env('DB_SERVER_VERSION', '11g'),
     ],
 ];
