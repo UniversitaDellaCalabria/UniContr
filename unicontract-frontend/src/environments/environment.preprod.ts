89dc0f4717c77a0621e5daf4e53f9ddfac3ede8b
Francesco Filicetti <francesco.filicetti@unical.it>
2022-10-17 11:02:50 +0200

    fix: env preprod

--- unicontract-frontend/src/environments/environment.preprod.ts
+++ unicontract-frontend/src/environments/environment.preprod.ts
@@ -1,8 +1,8 @@
 export const environment = {
-    production: true,
-    API_URL: 'https://unicontr.unical.it/public/',
-    baseHref: '/',
-    whitelistedDomains: ['unicontr.unical.it'],
-    blacklistedRoutes: ['https://unicontr.unical.it/public/auth/'],
-    documentation: 'https://unidem.uniurb.it/unicontr/documentation/'
+  production: false,
+  API_URL: '',
+  baseHref: '/',
+  whitelistedDomains: ['unicontr-preprod.unical.it'],
+  blacklistedRoutes: [''],
+  documentation: '',
 };
