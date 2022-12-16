0e56640e29359d99a66773d67f830c919902ee3a
Francesco Filicetti <francesco.filicetti@unical.it>
2022-12-16 14:33:21 +0100

    feat: redirect components

---
+++ unicontract-frontend/src/app/components/redirect_faq/redirect_faq.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "redirect_faq",
+})
+
+export class Redirect_FAQ implements OnInit {
+  constructor() {}
+
+  ngOnInit() {
+    window.location.href = 'https://docs.google.com/document/d/1UioNHVdc1lIAP1okcrhjR6CXKtzHuhVBIhg26Js6yqg/edit?usp=sharing';
+  }
+}
