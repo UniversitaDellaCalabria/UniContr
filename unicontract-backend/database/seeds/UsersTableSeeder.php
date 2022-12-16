0739b4fdc62bc053141f1be36224c72bbc1d0e9a
Francesco Filicetti <francesco.filicetti@unical.it>
2022-12-16 13:20:47 +0100

    fix: users seed

--- unicontract-backend/database/seeds/UsersTableSeeder.php
+++ unicontract-backend/database/seeds/UsersTableSeeder.php
@@ -18,8 +18,8 @@ class UsersTableSeeder extends Seeder
                 'email' => 'francesco.filicetti@unical.it' ,
                 'password' => Hash::make( 'test' ) ,
                 'name' => 'Francesco Filicetti' ,
-                'v_ie_ru_personale_id_ab'=> 030076,
-                'cf' => '1111111111111111',
+                'v_ie_ru_personale_id_ab'=> 48908,
+                'cf' => 'ffffff00f00f000f',
                 'nome' => 'Francesco',
                 'cognome' => 'Filicetti'
             ] );
@@ -34,8 +34,8 @@ class UsersTableSeeder extends Seeder
                 'email' => 'giuseppe.rossi@unical.it' ,
                 'password' => Hash::make( 'testrossi' ) ,
                 'name' => 'Giuseppe Rossi' ,
-                'v_ie_ru_personale_id_ab'=> 010373,
-                'cf' => '1111111111111112',
+                'v_ie_ru_personale_id_ab'=> 5226,
+                'cf' => 'rrrrrr00r00r000r',
                 'nome' => 'Giuseppe',
                 'cognome' => 'Rossi'
             ] );
@@ -44,35 +44,68 @@ class UsersTableSeeder extends Seeder
             $user->assignRole('super-admin');
         }
 
-        $user = User::where('email', 'test.admin@unical.it')->first();
+        $user = User::where('email', 'paola.cordiale@unical.it')->first();
         if ($user==null) {
             $user = User::firstOrCreate( [
-                'email' => 'test.admin@unical.it' ,
-                'password' => Hash::make( 'testadm1n' ) ,
-                'name' => 'test admin' ,
-                'cf' => '12346789LLLLLLL',
-                'nome' => 'test',
-                'cognome' => 'admin'
-                //'v_ie_ru_personale_id_ab'=> 39842,
+                'email' => 'paola.cordiale@unical.it' ,
+                'password' => Hash::make( 'testcordiale' ) ,
+                'name' => 'Paola Cordiale' ,
+                'v_ie_ru_personale_id_ab'=> 5036,
+                'cf' => 'cccccc00c00c000c',
+                'nome' => 'Paola',
+                'cognome' => 'Cordiale'
             ] );
         }
-        if (!$user->hasRole('admin')){
-            $user->assignRole('admin');
+        if (!$user->hasRole('op_approvazione_economica')){
+            $user->assignRole('op_approvazione_economica');
         }
 
-        $user = User::where('email', 'test.user@unical.it')->first();
-        if ($user==null){
-            $user = User::firstOrCreate([
-                'email' => 'test.user@unical.it' ,
-                'password' => Hash::make( 'testuser' ) ,
-                'name' => 'test user' ,
-                'cf' => '12346789LLLLLLL',
-                'nome' => 'test',
-                'cognome' => 'user'
+        $user = User::where('email', 'daniela.pisani@unical.it')->first();
+        if ($user==null) {
+            $user = User::firstOrCreate( [
+                'email' => 'daniela.pisani@unical.it' ,
+                'password' => Hash::make( 'testpisani' ) ,
+                'name' => 'Daniela Pisani' ,
+                'v_ie_ru_personale_id_ab'=> 6544,
+                'cf' => 'pppppp00p00p000p',
+                'nome' => 'Daniela',
+                'cognome' => 'Pisani'
             ] );
         }
-        if (!$user->hasRole('viewer')){
-            $user->assignRole('viewer');
+        if (!$user->hasRole('op_approvazione_amm')){
+            $user->assignRole('op_approvazione_amm');
+        }
+
+        $user = User::where('email', 'egidio.cario@unical.it')->first();
+        if ($user==null) {
+            $user = User::firstOrCreate( [
+                'email' => 'egidio.cario@unical.it' ,
+                'password' => Hash::make( 'testcario' ) ,
+                'name' => 'Egidio Cario' ,
+                'v_ie_ru_personale_id_ab'=> 5686,
+                'cf' => 'eeeeee00e00e000e',
+                'nome' => 'Egidio',
+                'cognome' => 'Cario'
+            ] );
+        }
+        if (!$user->hasRole('op_approvazione_amm')){
+            $user->assignRole('op_approvazione_amm');
+        }
+
+        $user = User::where('email', 'silvia.pagano@unical.it')->first();
+        if ($user==null) {
+            $user = User::firstOrCreate( [
+                'email' => 'silvia.pagano@unical.it' ,
+                'password' => Hash::make( 'testpag' ) ,
+                'name' => 'Silvia Pagano' ,
+                'v_ie_ru_personale_id_ab'=> 9603,
+                'cf' => 'ssssss00e00e000e',
+                'nome' => 'Silvia',
+                'cognome' => 'Pagano'
+            ] );
+        }
+        if (!$user->hasRole('op_approvazione_amm')){
+            $user->assignRole('op_approvazione_amm');
         }
 
     }
