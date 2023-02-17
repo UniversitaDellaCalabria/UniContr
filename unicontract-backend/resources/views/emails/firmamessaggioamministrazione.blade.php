50ed0e7b781545420b9580fb0762ebf55146e3d3
Francesco Filicetti <francesco.filicetti@unical.it>
2023-02-17 10:57:01 +0100

    fix: email text

--- unicontract-backend/resources/views/emails/firmamessaggioamministrazione.blade.php
+++ unicontract-backend/resources/views/emails/firmamessaggioamministrazione.blade.php
@@ -1,6 +1,6 @@
-Università della Calabria Ufficio<br>
-Direzione Risorse Umane<br>
+Università della Calabria<br>
+Ufficio Direzione Risorse Umane<br>
 Responsabile: Dott. Roberto Elmo<br>
 Assistenza alla compilazione: https://ticket.unical.it/tickets/new/17/443/<br>
 Assistenza problemi tecnici: https://ticket.unical.it/tickets/new/17/444/<br>
-(accesso con le credenziali di ateneo)
+(accesso con le credenziali di Ateneo)
