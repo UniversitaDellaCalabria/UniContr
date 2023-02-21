37d2c72e9cfc3331a99a5caa1714bc5727c3662f
Francesco Filicetti <francesco.filicetti@unical.it>
2023-02-21 16:47:02 +0100

    fix: bug in blade

--- unicontract-backend/resources/views/pdfModelloConflittoInteressiDipartimento.blade.php
+++ unicontract-backend/resources/views/pdfModelloConflittoInteressiDipartimento.blade.php
@@ -13,8 +13,8 @@ VISTO l’art. 53 D. Lgs. n. 165/2001, come modificato dalla legge n. 190/2012, 
 </p>
 
 <p class="normal">
-VISTO il curriculum, nonché la dichiarazione di assenza di conflitto di interessi resa, ai sensi dell’art. 53, comma 14, del D. Lgs. n. 165/2001, da {{$pre->genere['str5']} in relazione al seguente incarico:<br>
-Incarico di insegnamento {{$pre->insegnamentoDescr}} - SSD {{$pre->settore}} - Corso di {{$pre->tipoCorsoDes}} in {{$pre->cdl}} - Anno di corso {{$pre->annoCorso}} - Semestre {{$pre->periodo}} - per {{$pre->ore}} ore complessive ({{$pre->oreDesc}}) - A.A. {{$pre->aa}}
+VISTO il curriculum, nonché la dichiarazione di assenza di conflitto di interessi resa, ai sensi dell’art. 53, comma 14, del D. Lgs. n. 165/2001, da {{ $pre->genere['str5'] }} in relazione al seguente incarico:<br>
+Incarico di insegnamento {{ $pre->insegnamentoDescr }} - SSD {{ $pre->settore }} - Corso di {{ $pre->tipoCorsoDes }} in {{ $pre->cdl }} - Anno di corso {{ $pre->annoCorso }} - Semestre {{ $pre->periodo }} - per {{ $pre->ore }} ore complessive ({{ $pre->oreDesc }}) - A.A. {{$pre->aa}}
 <br>
 stabilito con {{ $pre->insegnamento->deliberaString() }}
 </p>
