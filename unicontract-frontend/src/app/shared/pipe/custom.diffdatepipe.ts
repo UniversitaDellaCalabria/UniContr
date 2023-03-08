fbce7ddd56f45e179d50abc65e7dd189a1db379f
Francesco Filicetti <francesco.filicetti@unical.it>
2023-03-08 11:26:27 +0100

    fix: MyDiffdatePipe with multiple dates

--- unicontract-frontend/src/app/shared/pipe/custom.diffdatepipe.ts
+++ unicontract-frontend/src/app/shared/pipe/custom.diffdatepipe.ts
@@ -4,22 +4,22 @@ import ControlUtils from "../dynamic-form/control-utils";
 import { DatePipe } from "@angular/common";
 
 @Pipe({
-    name: 'mydiffdate',   
+    name: 'mydiffdate',
   })
- 
+
 export class MyDiffdatePipe implements PipeTransform  {
-  
-  
-  public transform(value): number {    
+
+
+  public transform(value): number {
     //format yyyy-mm-aa
-    const dateFrom = new Date(value);
+    const dateFrom = new Date(value.split("#")[0]);
     const dateTo = new Date();
     if (dateFrom && dateTo){
       const diff = dateTo.valueOf() - dateFrom.valueOf();
       const diffDays = Math.floor(diff / (1000 * 3600 * 24));
       if (Number.isInteger(diffDays)){
         return diffDays + 1;
-      }       
+      }
     }
   }
 }
