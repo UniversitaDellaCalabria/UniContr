75f64f99c5b33b0ad44e18fc680948b5ae14df6e
Francesco Filicetti <easyweb.wa@gmail.com>
2025-09-25 10:30:36 +0200

    fix: unical config

--- unicontract-backend/config/unical.php
+++ unicontract-backend/config/unical.php
@@ -48,7 +48,7 @@ return [
     /**
      * Lista email separate da , per spedizione report alle segreterie
      */
-    'cc_report_segreterie' =>  explode(',',env('CC_REPORT_SEGRETERIE', 'francesco.filicetti@unical.it,giuliana.gabrieli@unical.it,giuseppe.rossi@unical.it')),
+    'cc_report_segreterie' =>  explode(',',env('CC_REPORT_SEGRETERIE', 'francesco.filicetti@unical.it')),
 
     'dibest_report_segreterie' =>  array_map('trim',explode(',',env('DIBEST_REPORT_SEGRETERIE', 'r.zicarelli@unical.it,emanuele.dodaro@unical.it'))),
     'ctc_report_segreterie' =>  array_map('trim',explode(',',env('CTC_REPORT_SEGRETERIE', 'giovanna.bonadies@unical.it'))),
@@ -84,7 +84,7 @@ return [
     /**
      * Lista email separate da , per notifica di visione accettazione da parte del docente
      */
-    'firma_direttore_email' => explode(',',env('FIRMA_DIRETTORE_EMAIL',  'roberto.elmo@unical.it,giuseppe.rossi@unical.it')),
+    'firma_direttore_email' => explode(',',env('FIRMA_DIRETTORE_EMAIL',  'roberto.elmo@unical.it')),
 
      /**
      * Lista email separate da , per notifica compilazione terminata da parte del docente
